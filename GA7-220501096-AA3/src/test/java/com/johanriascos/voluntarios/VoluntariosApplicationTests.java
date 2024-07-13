564cc2e4d510cdb569be906d9bf7b967342b21fa
Johan Riascos <desarrollador7777@gmail.com>
2024-07-13 12:50:07 -0500

    - Codificar los módulos del software Stand-alone, web y móvil --En Desarrollo

---
+++ GA7-220501096-AA3/src/test/java/com/johanriascos/voluntarios/VoluntariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanriascos.voluntarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
